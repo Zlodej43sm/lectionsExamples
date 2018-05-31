5c19653bbb4c427597231fed85a1f94fe6dc47a0
Alec Povolotskiy <aleksey.povolotskiy@vaimo.com>
2018-05-31 18:53:52 +0300

    l31 up

--- projects--node/express__app/app.js
+++ projects--node/express__app/app.js
@@ -1,17 +1,119 @@
-const mongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient, ObjectId } = require("mongodb");
 
+const app = express();
+const jsonParser = bodyParser.json();
 const url = "mongodb://localhost:27017/";
 
-mongoClient.connect(url, function(err, client){
-    const db = client.db("usersdb");
-    const collection = db.collection("users");
+app.use(express.static(__dirname + "/public"));
 
-    const user = {name: "Tom", age: 23};
-
-    collection.insertOne(user, function(err, result){
-        if(err) return console.log(err);
-
-        console.log(result.ops);
-        client.close();
+app.get("/api/users", function(req, res) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+        client.db("usersdb")
+            .collection("users")
+            .find()
+            .toArray(function(err, users) {
+                res.send(users);
+                client.close();
+            });
     });
+});
+
+app.get("/api/users/:id", function(req, res){
+    const id = new ObjectId(req.params.id);
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+        client.db("usersdb")
+            .collection("users")
+            .findOne(
+                {_id: id},
+                function(err, user){
+                    if(err) return res.status(400).send();
+
+                    res.send(user);
+                    client.close();
+                }
+            );
+    });
+});
+
+app.post("/api/users", jsonParser, function (req, res) {
+    if(!req.body) return res.sendStatus(400);
+
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = {name: userName, age: userAge};
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+        client.db("usersdb")
+            .collection("users")
+            .insertOne(
+                user,
+                function(err, result) {
+                    if(err) return res.status(400).send();
+
+                    res.send(user);
+                    client.close();
+                }
+            );
+    });
+});
+
+app.delete("/api/users/:id", function(req, res){
+    const id = new ObjectId(req.params.id);
+
+    MongoClient.connect(url, { useNewUrlParser: true },  function(err, client){
+        client.db("usersdb")
+            .collection("users")
+            .findOneAndDelete(
+                {_id: id},
+                function(err, result) {
+                    if(err) return res.status(400).send();
+
+                    const user = result.value;
+
+                    res.send(user);
+                    client.close();
+                }
+            );
+    });
+});
+
+app.put("/api/users", jsonParser, function(req, res){
+    if(!req.body) return res.sendStatus(400);
+
+    const id = new ObjectId(req.body._id);
+    const userName = req.body.name;
+    const userAge = req.body.age;
+console.log(id);
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client){
+        client.db("usersdb")
+            .collection("users")
+            .findOneAndUpdate(
+                {_id: id},
+                {
+                    $set: {
+                        age: userAge,
+                        name: userName
+                    }
+                },
+                {
+                    returnOriginal: false
+                },
+                function(err, result){
+
+                    if(err) return res.status(400).send();
+
+                    const user = result.value;
+
+                    res.send(user);
+                    client.close();
+                }
+            );
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Сервер ожидает подключения...");
 });
